61766d8f418fc13399b34cede88dcd94bc73a8ad
markdegroat <mhdegroat@gmail.com>
2017-04-27 11:41:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 'use strict';
+// Dog with name, breed, and age properties
+// Cat with name, breed, and age properties
+// Ferret with name, age, and foodRequirements properties
+// Bird with name and breed properties
+// Fish with name, species, and waterConditions properties
+
+
+function Dog(name, breed, age){
+  this.name = name
+  this.breed = breed
+  this.age = age
+}
+
+function Cat(name, breed, age){
+  this.name = name
+  this.breed = breed
+  this.age = age
+}
+
+function Ferret(name, age, foodRequirements){
+  this.name = name
+  this.foodRequirements = foodRequirements
+  this.age = age
+}
+
+function Bird(name, breed){
+  this.name = name
+  this.breed = breed
+}
+
+function Fish(name, species, waterConditions){
+  this.name = name
+  this.waterConditions = waterConditions
+  this.species = species
+}
